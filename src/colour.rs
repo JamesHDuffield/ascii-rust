a21cc351086946435d423120ff958432fd81dd41
James Duffield <james.h.duffield@gmail.com>
2023-04-19 23:21:54 +1000

    feat: :sparkles: add the enemy colour

--- src/colour.rs
+++ src/colour.rs
@@ -5,3 +5,4 @@ pub const PLAYER: Color = Color::Rgba { red: 168.0 / 255.0, green: 207.0 / 255.0
 pub const WHITE: Color = Color::Rgba { red: 238.0 / 255.0, green: 236.0 / 255.0, blue: 222.0 / 255.0, alpha: 1.0 };
 pub const SHIELD: Color = Color::Rgba { red: 120.0 / 255.0, green: 149.0 / 255.0, blue: 171.0 / 255.0, alpha: 1.0 };
 pub const INACTIVE: Color = Color::Rgba { red: 119.0 / 255.0, green: 117.0 / 255.0, blue: 103.0 / 255.0, alpha: 1.0 };
+pub const ENEMY: Color = Color::Rgba { red: 172.0 / 255.0, green: 138.0 / 255.0, blue: 113.0 / 255.0, alpha: 1.0 };
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,22 @@
 mod colour;
-mod math;
 mod component;
+mod math;
 mod system;
 
-use std::f32::consts::PI;
+use bevy::{core_pipeline::clear_color::ClearColorConfig, prelude::*};
 use component::*;
+use std::f32::consts::PI;
 use system::*;
-use bevy::{prelude::*, core_pipeline::clear_color::ClearColorConfig};
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins.set(WindowPlugin { primary_window: Some(Window { title: String::from("Outer Shell"), ..Default::default() }), ..Default::default() }))
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: String::from("Outer Shell"),
+                ..Default::default()
+            }),
+            ..Default::default()
+        }))
         .add_startup_system(setup)
         .add_system(ui_system)
         .add_system(physics_system)
@@ -24,96 +30,142 @@ fn main() {
 }
 
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
-    let font = asset_server.load("fonts/AnonymousPro-Regular.ttf");
-    let text_style = TextStyle {
-        font: font.clone(),
-        font_size: 40.0,
-        color: colour::PLAYER,
-    };
     // Spawn the Camera
-    commands.spawn((Camera2dBundle {
-        camera_2d: Camera2d {
-            clear_color: ClearColorConfig::Custom(colour::BLACK),
+    commands.spawn((
+        Camera2dBundle {
+            camera_2d: Camera2d {
+                clear_color: ClearColorConfig::Custom(colour::BLACK),
+            },
+            ..Default::default()
         },
-        ..Default::default()
-    }, MainCamera));
+        MainCamera,
+    ));
     // Spawn the player
-    commands.spawn((
-        Text2dBundle {
-            text: Text::from_section("V", text_style.clone()).with_alignment(TextAlignment::Center),
-            transform: Transform { translation: Vec3 { x: 100.0, y: 100.0, z: 0.0 }, scale: Vec3 { x: 0.5, y: 0.5, z: 1.0 }, ..default() },
-            ..default()
-        },
-        BaseGlyphRotation { rotation: Quat::from_rotation_z(PI / 2.0) },
-        IsPlayer,
-        Physics::new(5.0),
-        Engine::new(10.0, 20.0),
-        Health::new(100, 100),
-        Collider { radius: 5.0 },
-    )).with_children(|parent| {
-        parent.spawn(Turret::new(5.0, 200.0));
-    });
-
-    // Spawn an enemy
-    commands.spawn((
-        Text2dBundle {
-            text: Text::from_section("w", text_style.clone()).with_alignment(TextAlignment::Center),
-            transform: Transform { translation: Vec3 { x: -100.0, y: -100.0, z: 0.0 }, scale: Vec3 { x: 0.5, y: 0.5, z: 1.0 }, ..default() },
-            ..default()
-        },
-        BaseGlyphRotation { rotation: Quat::from_rotation_z(PI / 2.0) },
-        Physics::new(5.0),
-        Engine::new(10.0, 10.0),
-        Health::new(60, 20),
-        Collider { radius: 5.0 },
-    )).with_children(|parent| {
-        parent.spawn(Turret::new(1.0, 200.0));
-    });
-
-    // UI
-    commands.spawn((
-        NodeBundle {
-            style: Style {
-                size: Size { width: Val::Percent(20.0), height: Val::Percent(20.0)},
-                margin: UiRect::all(Val::Px(5.0)),
-                gap: Size { height: Val::Px(2.0), ..Default::default() },
-                flex_direction: FlexDirection::Column,
+    commands
+        .spawn((
+            Text2dBundle {
+                text: Text::from_section(
+                    "V",
+                    TextStyle {
+                        font: asset_server.load("fonts/AnonymousPro-Regular.ttf"),
+                        font_size: 40.0,
+                        color: colour::PLAYER,
+                    },
+                )
+                .with_alignment(TextAlignment::Center),
+                transform: Transform {
+                    translation: Vec3 {
+                        x: 100.0,
+                        y: 100.0,
+                        z: 0.0,
+                    },
+                    scale: Vec3 {
+                        x: 0.5,
+                        y: 0.5,
+                        z: 1.0,
+                    },
+                    ..default()
+                },
                 ..default()
             },
-            ..default()
-        },
-        UINode,
-    )).with_children(|parent| {
-        parent.spawn(
-            TextBundle::from_section(
+            BaseGlyphRotation {
+                rotation: Quat::from_rotation_z(PI / 2.0),
+            },
+            IsPlayer,
+            Physics::new(5.0),
+            Engine::new(10.0, 20.0),
+            Health::new(100, 100),
+            Collider { radius: 5.0 },
+        ))
+        .with_children(|parent| {
+            parent.spawn(Turret::new(5.0, 200.0));
+        });
+
+    // Spawn an enemy
+    commands
+        .spawn((
+            Text2dBundle {
+                text: Text::from_section(
+                    "w",
+                    TextStyle {
+                        font: asset_server.load("fonts/AnonymousPro-Regular.ttf"),
+                        font_size: 32.0,
+                        color: colour::ENEMY,
+                    },
+                )
+                .with_alignment(TextAlignment::Center),
+                transform: Transform {
+                    translation: Vec3 {
+                        x: -100.0,
+                        y: -100.0,
+                        z: 0.0,
+                    },
+                    scale: Vec3 {
+                        x: 0.5,
+                        y: 0.5,
+                        z: 1.0,
+                    },
+                    ..default()
+                },
+                ..default()
+            },
+            BaseGlyphRotation {
+                rotation: Quat::from_rotation_z(PI / 2.0),
+            },
+            Physics::new(5.0),
+            Engine::new(10.0, 10.0),
+            Health::new(60, 20),
+            Collider { radius: 5.0 },
+        ))
+        .with_children(|parent| {
+            parent.spawn(Turret::new(1.0, 200.0));
+        });
+
+    // UI
+    commands
+        .spawn((
+            NodeBundle {
+                style: Style {
+                    size: Size {
+                        width: Val::Percent(20.0),
+                        height: Val::Percent(20.0),
+                    },
+                    margin: UiRect::all(Val::Px(5.0)),
+                    gap: Size {
+                        height: Val::Px(2.0),
+                        ..Default::default()
+                    },
+                    flex_direction: FlexDirection::Column,
+                    ..default()
+                },
+                ..default()
+            },
+            UINode,
+        ))
+        .with_children(|parent| {
+            parent.spawn(TextBundle::from_section(
                 "Health",
                 TextStyle {
                     font: asset_server.load("fonts/AnonymousPro-Regular.ttf"),
                     font_size: 12.0,
                     color: colour::WHITE,
                 },
-            ),
-        );
-        parent.spawn(
-            TextBundle::from_section(
+            ));
+            parent.spawn(TextBundle::from_section(
                 "Shield",
                 TextStyle {
                     font: asset_server.load("fonts/AnonymousPro-Regular.ttf"),
                     font_size: 12.0,
                     color: colour::SHIELD,
                 },
-            ),
-        );
-        parent.spawn(
-            TextBundle::from_section(
+            ));
+            parent.spawn(TextBundle::from_section(
                 "Engine",
                 TextStyle {
                     font: asset_server.load("fonts/AnonymousPro-Regular.ttf"),
                     font_size: 12.0,
                     color: colour::INACTIVE,
                 },
-            ),
-        );
-    });
+            ));
+        });
 }
-
