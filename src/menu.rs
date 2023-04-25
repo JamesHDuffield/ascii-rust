f3788d49d28e02878c9503d0a2cd38966cb920db
James Duffield <james.h.duffield@gmail.com>
2023-04-25 16:00:42 +1000

    feat: :sparkles: add multiple main menu items and exit

--- src/menu.rs
+++ src/menu.rs
@@ -1,10 +1,19 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, app::AppExit};
 
-use crate::{AppState, resource::Fonts};
+use crate::{resource::Fonts, AppState};
 
 #[derive(Resource)]
 struct MainMenuData {
-    button_entity: Entity,
+    root_entity: Entity,
+}
+
+#[derive(Component)]
+struct MainMenuButton(pub ButtonAction);
+
+enum ButtonAction {
+    Play,
+    Options,
+    Exit,
 }
 
 const HOVERED_BUTTON: Color = Color::rgb(0.25, 0.25, 0.25);
@@ -14,76 +23,90 @@ pub struct MainMenuPlugin;
 
 impl Plugin for MainMenuPlugin {
     fn build(&self, app: &mut App) {
-        app
-         .add_system(setup_menu.in_schedule(OnEnter(AppState::Menu)))
-         .add_system(menu.in_set(OnUpdate(AppState::Menu)))
-         .add_system(cleanup_menu.in_schedule(OnExit(AppState::Menu)));
+        app.add_system(setup_menu.in_schedule(OnEnter(AppState::Menu)))
+            .add_system(menu.in_set(OnUpdate(AppState::Menu)))
+            .add_system(cleanup_menu.in_schedule(OnExit(AppState::Menu)));
     }
 }
 
 fn setup_menu(mut commands: Commands, fonts: Res<Fonts>) {
-  let button_entity = commands
-      .spawn(NodeBundle {
-          style: Style {
-              // center button
-              size: Size::new(Val::Percent(100.0), Val::Percent(100.0)),
-              justify_content: JustifyContent::Center,
-              align_items: AlignItems::Center,
-              ..default()
-          },
-          ..default()
-      })
-      .with_children(|parent| {
-          parent
-              .spawn(ButtonBundle {
-                  style: Style {
-                      size: Size::new(Val::Px(150.0), Val::Px(65.0)),
-                      // horizontally center child text
-                      justify_content: JustifyContent::Center,
-                      // vertically center child text
-                      align_items: AlignItems::Center,
-                      ..default()
-                  },
-                  background_color: NORMAL_BUTTON.into(),
-                  ..default()
-              })
-              .with_children(|parent| {
-                  parent.spawn(TextBundle::from_section(
-                      "Play",
-                      TextStyle {
-                          font: fonts.primary.clone(),
-                          font_size: 40.0,
-                          color: Color::rgb(0.9, 0.9, 0.9),
-                      },
-                  ));
-              });
-      })
-      .id();
-  commands.insert_resource(MainMenuData { button_entity });
+    let root_entity = commands
+        .spawn(NodeBundle {
+            style: Style {
+                // center button
+                size: Size::new(Val::Percent(100.0), Val::Percent(100.0)),
+                justify_content: JustifyContent::Center,
+                align_items: AlignItems::Center,
+                flex_direction: FlexDirection::Column,
+                gap: Size::height(Val::Px(10.0)),
+                ..default()
+            },
+            ..default()
+        })
+        .with_children(|parent| {
+            button(parent, &fonts, "Play", ButtonAction::Play);
+            button(parent, &fonts, "Options", ButtonAction::Options);
+            button(parent, &fonts, "Exit", ButtonAction::Exit);
+        })
+        .id();
+    commands.insert_resource(MainMenuData { root_entity });
 }
 
 fn menu(
-  mut next_state: ResMut<NextState<AppState>>,
-  mut interaction_query: Query<
-      (&Interaction, &mut BackgroundColor),
-      (Changed<Interaction>, With<Button>),
-  >,
+    mut next_state: ResMut<NextState<AppState>>,
+    mut interaction_query: Query<
+        (&Interaction, &mut BackgroundColor, &MainMenuButton),
+        (Changed<Interaction>, With<Button>, With<MainMenuButton>),
+    >,
+    mut exit: EventWriter<AppExit>,
 ) {
-  for (interaction, mut color) in &mut interaction_query {
-      match *interaction {
-          Interaction::Clicked => {
-              next_state.set(AppState::InGame);
-          }
-          Interaction::Hovered => {
-              *color = HOVERED_BUTTON.into();
-          }
-          Interaction::None => {
-              *color = NORMAL_BUTTON.into();
-          }
-      }
-  }
+    for (interaction, mut color, button) in &mut interaction_query {
+        match *interaction {
+            Interaction::Clicked => {
+                match button.0 {
+                    ButtonAction::Play => next_state.set(AppState::InGame),
+                    ButtonAction::Options => (),
+                    ButtonAction::Exit => exit.send(AppExit),
+                }
+            }
+            Interaction::Hovered => {
+                *color = HOVERED_BUTTON.into();
+            }
+            Interaction::None => {
+                *color = NORMAL_BUTTON.into();
+            }
+        }
+    }
 }
 
 fn cleanup_menu(mut commands: Commands, menu_data: Res<MainMenuData>) {
-  commands.entity(menu_data.button_entity).despawn_recursive();
+    commands.entity(menu_data.root_entity).despawn_recursive();
+}
+
+fn button(parent: &mut ChildBuilder, fonts: &Res<Fonts>, text: &str, action: ButtonAction) {
+    parent
+        .spawn((
+            ButtonBundle {
+                style: Style {
+                    min_size: Size::width(Val::Px(200.0)),
+                    justify_content: JustifyContent::Center,
+                    align_items: AlignItems::Center,
+                    padding: UiRect::all(Val::Px(10.0)),
+                    ..default()
+                },
+                background_color: NORMAL_BUTTON.into(),
+                ..default()
+            },
+            MainMenuButton(action),
+        ))
+        .with_children(|parent| {
+            parent.spawn(TextBundle::from_section(
+                text,
+                TextStyle {
+                    font: fonts.primary.clone(),
+                    font_size: 40.0,
+                    color: Color::rgb(0.9, 0.9, 0.9),
+                },
+            ));
+        });
 }
